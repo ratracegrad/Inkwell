12068b42f3cf67e9de76e9073fbaca9e5230b042
Siddharth Sukumar <siddharth@inauth.com>
2015-05-14 10:01:19 -0700

    Set up karma for testing, added a few tests in the test folder

---
+++ test/integration/apiTests.js
@@ -0,0 +1,28 @@
+var assert = chai.assert;
+var should = chai.should();
+var expect = chai.expect;
+
+
+// describe('API integration', function(){
+//     var server, setupStub, JSONresponse, responseComeback;
+//     var server = sinon.fakeServer.create();
+//     server.respondWith("GET", "/twitter/api/user.json", [
+//     200, 
+//     {"Content-Type": "application/json"}, 
+//     '[{"id": 0, "tweet": "Hello World"}]'
+// ]);
+ 
+// $.get("/twitter/api/user.json", function (data) {
+//     responseComeback = data;
+//     console.log(data); // [{"id":0,"tweet":"Hello World"}] 
+// });
+ 
+
+// server.respond();
+ 
+// server.restore();
+
+
+  
+
+// });
