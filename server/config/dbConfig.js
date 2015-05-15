2f4cab61a2b41f28a5992a52eff60ebf7182cac4
Jennifer Bland <ratracegrad@gmail.com>
2015-05-14 22:38:38 -0400

    Added update to config file

--- server/config/dbConfig.js
+++ server/config/dbConfig.js
@@ -5,9 +5,13 @@ var Sequelize = require('sequelize');
 // go for it.  Check out Sequelize's docs for more info:
 // http://sequelize.readthedocs.org/en/latest/
 
-var sequelize = new Sequelize('d2evge8hbplo49', 'uocrcyaynwlxdj', 'SbC7NE7Qph_ca3FZYtFOgSWsXu', {
-  host: 'postgres://uocrcyaynwlxdj:SbC7NE7Qph_ca3FZYtFOgSWsXu@ec2-107-20-152-139.compute-1.amazonaws.com:5432/d2evge8hbplo49',
-  dialect: 'postgres'
-});
+if (process.env.DATABASE_URL) {
+  var sequelize = new Sequelize(process.env.DATABASE_URL);
+} else {
+  var sequelize = new Sequelize('websteamcmd', 'root', '', {
+    host: 'localhost',
+    dialect: 'mysql'
+  });
+}
 
 module.exports = sequelize;
